f2c35d9198f842935b6cbf3c3521d5a434933252
Aakshayyy <150322750+Aakshayyy@users.noreply.github.com>
2026-05-30 01:33:44 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define
+define('LARAVEL_START', microtime(true));
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -17,8 +18,5 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
-('LARAVEL_START', microtime(true));
-
-
 // echo "<h1>Website Under Maintenance</h1>";
 // exit;
